6785356d5b0ca271ba75b79e56936f737be2d85c
DeadDuck <git@dead0duck.ru>
2024-12-04 10:30:20 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,7 +30,7 @@ TZ=${TZ:-UTC}
 export TZ
 
 # Set environment variable that holds the Internal Docker IP
-INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+INTERNAL_IP=$(hostname -I)
 export INTERNAL_IP
 
 # Switch to the container's working directory
